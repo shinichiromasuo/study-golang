8540ced5ac330b3eabebe292882091056031356c
masuoshinichiro <masuo@wh-plus.com>
2019-05-14 11:43:01 +0900

    chap1 q2 mutipartform x-www-form-urlencoded json の受け取りの制御

--- chap1/main.go
+++ chap1/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
 )
 
+type User struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
 func main() {
-	http.HandleFunc("/main", handlerOnlyPost)
+	http.Handle("/asset/", http.FileServer(http.Dir("../")))
+	http.HandleFunc("/main", handlerMain)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handlerOnlyPost(w http.ResponseWriter, r *http.Request) {
+func handlerMain(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodGet {
 		w.Write([]byte("get request \n"))
@@ -19,9 +29,30 @@ func handlerOnlyPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPost {
-		w.Write([]byte("post request \n"))
-		log.Printf("get request")
-		return
+
+		if r.Header.Get("Content-Type") == "application/json" {
+			dec := json.NewDecoder(r.Body)
+			var reqJson map[string]interface{}
+			dec.Decode(&reqJson)
+			fmt.Printf("%v\n", reqJson)
+
+			return
+		}
+
+		if strings.Contains(r.Header.Get("Content-type"), "multipart/form-data") {
+			mulForm := r.FormValue("aaa")
+			fmt.Println(mulForm)
+
+			return
+		}
+
+		if r.Header.Get("Content-type") == "application/x-www-form-urlencoded" {
+			xform := r.FormValue("aaa")
+			fmt.Println(xform)
+
+			return
+		}
+
 	}
 
 	w.Write([]byte("Other than GET,POST Request \n"))
